68d5a5626fcb6d3cf05706c91eeb1e6b4af49c08
gauravcg492 <officialgauravcg492@gmail.com>
2021-02-19 15:04:07 +0530

    few modifications done

--- todo-backend/src/main/java/com/todo/todobackend/security/SecurityConfig.java
+++ todo-backend/src/main/java/com/todo/todobackend/security/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.todo.todobackend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.todo.todobackend.security.filters.JwtRequestFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	private JwtRequestFilter jwtReqFilter;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 //		http.authorizeRequests().antMatchers("/graphql").permitAll().antMatchers("/playground").permitAll().antMatchers("/vendor/**").permitAll().anyRequest().authenticated();
-		http.authorizeRequests().antMatchers("/graphql").permitAll().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/graphql").permitAll().anyRequest().permitAll();
+		http.addFilterBefore(jwtReqFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
--- todo-backend/src/main/java/com/todo/todobackend/security/filters/JwtRequestFilter.java
+++ todo-backend/src/main/java/com/todo/todobackend/security/filters/JwtRequestFilter.java
@@ -28,7 +28,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		final String authorization = request.getHeader("Authorization");
 		String email = null;
 		String token = null;
